9081ac664e439fec883b6d47bc23fa8fdf272c4a
Rich King <rking@rebgv.org>
2020-05-28 10:59:03 -0700

    Added stub classes for other tables

--- table_definitions.py
+++ table_definitions.py
@@ -6,6 +6,30 @@ base = declarative_base()
 # Each table we connect to needs to have a class scaffolding it.
 # You only need to define the columns that you need.
 
+class ClassPriceRule(base):
+    __tablename__ = 'TEMP_CLASS_PRICE_RULE'
+
+class ClassSession(base):
+    __tablename__ = 'TEMP_CLASS_SESSION'
+
+class Registration(base):
+    __tablename__ = 'TEMP_REGISTRATION'
+
+class ContactCourseDetail(base):
+    __tablename__ = 'TEMP_CONTACT_COURSE_DETAIL'
+
+class RegistrationComponent(base):
+    __tablename__ = 'TEMP_REGISTRATION_COMPONENT'
+
+class ClassAttendance(base):
+    __tablename__ = 'TEMP_CLASS_ATTENDANCE'
+
+class ClassAttendanceLog(base):
+    __tablename__ = 'TEMP_CLASS_ATTENDANCE_LOG'
+
+class EducationHistory(base):
+    __tablename__ = 'TEMP_EDUCATION_HISTORY'
+
 class NonComplianceNotification(base):
     __tablename__ = 'TEMP_NON_COMPLIANCE_NOTIFICATION'
 
