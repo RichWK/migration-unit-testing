d82908e9b20d258c6333babbf1166ecbbf042556
Rich King <rking@rebgv.org>
2020-05-28 15:05:11 -0700

    Built ClassPriceRule, ClassSession & Registration

--- table_definitions.py
+++ table_definitions.py
@@ -1,19 +1,80 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Float
 
 base = declarative_base()
 
-# Each table we connect to needs to have a class scaffolding it.
-# You only need to define the columns that you need.
+# Each table we connect to needs to have a class scaffolding it — otherwise SQLAlchemy
+# doesn't know what columns it contains.
 
 class ClassPriceRule(base):
     __tablename__ = 'TEMP_CLASS_PRICE_RULE'
 
+    classID = Column(String)
+    courseID = Column(String)
+    courseClassLevel = Column(Integer)
+    name = Column(String)
+    price = Column(Integer)
+    statusCode = Column(Integer)
+    hasThisLicenseLevel = Column(Integer)
+    usePriceListItem = Column(Integer)
+    priceListID = Column(String)
+    displayName = Column(String)
+
 class ClassSession(base):
     __tablename__ = 'TEMP_CLASS_SESSION'
 
+    attendanceGeneratedOn = Column(DateTime)
+    classID = Column(String)
+    courseID = Column(String)
+    courseClassLevel = Column(Integer)
+    endDate = Column(DateTime)
+    startDate = Column(DateTime)
+    statusCode = Column(Integer)
+    name = Column(String)
+    sessionNumber = Column(Integer)
+    classComponentID = Column(String)
+    displayName = Column(String)
+
 class Registration(base):
     __tablename__ = 'TEMP_REGISTRATION'
+
+    emailAddress = Column(String)
+    alternatePhoneNumber = Column(String)
+    cancelOrderID = Column(String)
+    cancelOverride = Column(Integer)
+    cancelReason = Column(Integer)
+    classID = Column(String)
+    classPrice = Column(Float)
+    contactID = Column(String)
+    counter = Column(Integer)
+    courseID = Column(String)
+    dateAddedToWaitlist = Column(DateTime)
+    dateCancelledTransferred = Column(DateTime)
+    dateCourseEvalSent = Column(DateTime)
+    dateRegistered = Column(DateTime)
+    dateRemovedFromWaitList = Column(DateTime)
+    contactCourseDetailID = Column(String)
+    hold = Column(Integer)
+    holdUntil = Column(DateTime)
+    isNotABoardMember = Column(Integer)
+    name = Column(String)
+    orderID = Column(Integer)
+    orderProductID = Column(Integer)
+    organizationID = Column(String)
+    overrideAllowCourseToBeTaken = Column(Integer)
+    preClassActivityEmailProcessed = Column(Integer)
+    priceRuleID = Column(String)
+    regID = Column(String)
+    registered = Column(Integer)
+    reasonForTaking = Column(Integer)
+    registrationSteps = Column(Integer)
+    transferFromRegID = Column(Integer)
+    transferPending = Column(Integer)
+    waitList = Column(Integer)
+    waitListStatus = Column(Integer)
+    statusReason = Column(Integer)
+    classStartDate = Column(DateTime)
+    displayName = Column(String)
 
 class ContactCourseDetail(base):
     __tablename__ = 'TEMP_CONTACT_COURSE_DETAIL'
