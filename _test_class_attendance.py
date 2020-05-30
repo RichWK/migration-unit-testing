6ac94eb23ec9865d62581048850b77e0e815fe47
Rich King <rking@rebgv.org>
2020-05-29 17:48:36 -0700

    Fixed bug resulting in incorrect evaluation

--- _test_class_attendance.py
+++ _test_class_attendance.py
@@ -1,50 +1,48 @@
 from program import *
 from table_definitions import *
 
-table = "ClassAttendance"
-results = Connection.session.query(eval(table))
-table += "."
+results = Connection.session.query(ClassAttendance)
 
 
 
 # These tests verify there are no null values in each column.
 
 def test_reason():
-    assert is_not_null(results, table + "reason") == None
+    assert is_not_null(results, ClassAttendance.reason) == True
 
 def test_passed():
-    assert is_not_null(results, table + "passed") == None
+    assert is_not_null(results, ClassAttendance.passed) == True
 
 def test_checkInDate():
-    assert is_not_null(results, table + "checkInDate") == None
+    assert is_not_null(results, ClassAttendance.checkInDate) == True
 
 def test_checkOutDate():
-    assert is_not_null(results, table + "checkOutDate") == None
+    assert is_not_null(results, ClassAttendance.checkOutDate) == True
 
 def test_classID():
-    assert is_not_null(results, table + "classID") == None
+    assert is_not_null(results, ClassAttendance.classID) == True
 
 def test_classSessionID():
-    assert is_not_null(results, table + "classSessionID") == None
+    assert is_not_null(results, ClassAttendance.classSessionID) == True
 
 def test_contactID():
-    assert is_not_null(results, table + "contactID") == None
+    assert is_not_null(results, ClassAttendance.contactID) == True
 
 def test_statusReason():
-    assert is_not_null(results, table + "statusReason") == None
+    assert is_not_null(results, ClassAttendance.statusReason) == True
 
 def test_name():
-    assert is_not_null(results, table + "name") == None
+    assert is_not_null(results, ClassAttendance.name) == True
 
 def test_attendanceNote():
-    assert is_not_null(results, table + "attendanceNote") == None
+    assert is_not_null(results, ClassAttendance.attendanceNote) == True
 
 def test_registrationComponent():
-    assert is_not_null(results, table + "registrationComponent") == None
+    assert is_not_null(results, ClassAttendance.registrationComponent) == True
 
 def test_registration():
-    assert is_not_null(results, table + "registration") == None
+    assert is_not_null(results, ClassAttendance.registration) == True
 
 def test_displayName():
-    assert is_not_null(results, table + "displayName") == None
+    assert is_not_null(results, ClassAttendance.displayName) == True
 
