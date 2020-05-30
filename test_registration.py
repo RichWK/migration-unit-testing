5b150e728632c52d0441b315965326ec9f3b3b99
Rich King <rking@rebgv.org>
2020-05-29 17:21:20 -0700

    FIxed bug with incorrect test results

--- test_registration.py
+++ test_registration.py
@@ -1,110 +1,120 @@
 from program import *
 from table_definitions import *
 
-table = "Registration"
-results = Connection.session.query(eval(table))
-table += "."
+results = Connection.session.query(Registration)
 
 
 
 # These tests verify there are no null values in each column.
 
 def test_emailAddress():
-    assert is_not_null(results, table + "emailAddress") == None
+    assert is_not_null(results, Registration.emailAddress) == None
 
 def test_alternatePhoneNumber():
-    assert is_not_null(results, table + "alternatePhoneNumber") == None
+    assert is_not_null(results, Registration.alternatePhoneNumber) == None
 
 def test_cancelOrderID():
-    assert is_not_null(results, table + "cancelOrderID") == None
+    assert is_not_null(results, Registration.cancelOrderID) == None
 
 def test_cancelOverride():
-    assert is_not_null(results, table + "cancelOverride") == None
+    assert is_not_null(results, Registration.cancelOverride) == None
 
 def test_cancelReason():
-    assert is_not_null(results, table + "cancelReason") == None
+    assert is_not_null(results, Registration.cancelReason) == None
 
 def test_classID():
-    assert is_not_null(results, table + "classID") == None
+    assert is_not_null(results, Registration.classID) == None
 
 def test_classPrice():
-    assert is_not_null(results, table + "classPrice") == None
+    assert is_not_null(results, Registration.classPrice) == None
 
 def test_contactID():
-    assert is_not_null(results, table + "contactID") == None
+    assert is_not_null(results, Registration.contactID) == None
 
 def test_counter():
-    assert is_not_null(results, table + "counter") == None
+    assert is_not_null(results, Registration.counter) == None
 
 def test_courseID():
-    assert is_not_null(results, table + "courseID") == None
+    assert is_not_null(results, Registration.courseID) == None
 
 def test_dateAddedToWaitlist():
-    assert is_not_null(results, table + "dateAddedToWaitlist") == None
+    assert is_not_null(results, Registration.dateAddedToWaitlist) == None
+
+def test_dateCancelledTransferred():
+    assert is_not_null(results, Registration.dateCancelledTransferred) == None
+
+def test_dateCourseEvalSent():
+    assert is_not_null(results, Registration.dateCourseEvalSent) == None
+
+def test_dateRegistered():
+    assert is_not_null(results, Registration.dateRegistered) == None
+
+def test_dateRemovedFromWaitList():
+    assert is_not_null(results, Registration.dateRemovedFromWaitList) == None
 
 def test_contactCourseDetailID():
-    assert is_not_null(results, table + "contactCourseDetailID") == None
+    assert is_not_null(results, Registration.contactCourseDetailID) == None
 
 def test_hold():
-    assert is_not_null(results, table + "hold") == None
+    assert is_not_null(results, Registration.hold) == None
 
 def test_holdUntil():
-    assert is_not_null(results, table + "holdUntil") == None
+    assert is_not_null(results, Registration.holdUntil) == None
 
 def test_isNotABoardMember():
-    assert is_not_null(results, table + "isNotABoardMember") == None
+    assert is_not_null(results, Registration.isNotABoardMember) == None
 
 def test_name():
-    assert is_not_null(results, table + "name") == None
+    assert is_not_null(results, Registration.name) == None
 
 def test_orderID():
-    assert is_not_null(results, table + "orderID") == None
+    assert is_not_null(results, Registration.orderID) == None
 
 def test_orderProductID():
-    assert is_not_null(results, table + "orderProductID") == None
+    assert is_not_null(results, Registration.orderProductID) == None
 
 def test_organizationID():
-    assert is_not_null(results, table + "organizationID") == None
+    assert is_not_null(results, Registration.organizationID) == None
 
 def test_overrideAllowCourseToBeTaken():
-    assert is_not_null(results, table + "overrideAllowCourseToBeTaken") == None
+    assert is_not_null(results, Registration.overrideAllowCourseToBeTaken) == None
 
 def test_preClassActivityEmailProcessed():
-    assert is_not_null(results, table + "preClassActivityEmailProcessed") == None
+    assert is_not_null(results, Registration.preClassActivityEmailProcessed) == None
 
 def test_priceRuleID():
-    assert is_not_null(results, table + "priceRuleID") == None
+    assert is_not_null(results, Registration.priceRuleID) == None
 
 def test_regID():
-    assert is_not_null(results, table + "regID") == None
+    assert is_not_null(results, Registration.regID) == None
 
 def test_registered():
-    assert is_not_null(results, table + "registered") == None
+    assert is_not_null(results, Registration.registered) == None
 
 def test_reasonForTaking():
-    assert is_not_null(results, table + "reasonForTaking") == None
+    assert is_not_null(results, Registration.reasonForTaking) == None
 
 def test_registrationSteps():
-    assert is_not_null(results, table + "registrationSteps") == None
+    assert is_not_null(results, Registration.registrationSteps) == None
 
 def test_transferFromRegID():
-    assert is_not_null(results, table + "transferFromRegID") == None
+    assert is_not_null(results, Registration.transferFromRegID) == None
 
 def test_transferPending():
-    assert is_not_null(results, table + "transferPending") == None
+    assert is_not_null(results, Registration.transferPending) == None
 
 def test_waitList():
-    assert is_not_null(results, table + "waitList") == None
+    assert is_not_null(results, Registration.waitList) == None
 
 def test_waitListStatus():
-    assert is_not_null(results, table + "waitListStatus") == None
+    assert is_not_null(results, Registration.waitListStatus) == None
 
 def test_statusReason():
-    assert is_not_null(results, table + "statusReason") == None
+    assert is_not_null(results, Registration.statusReason) == None
 
 def test_classStartDate():
-    assert is_not_null(results, table + "classStartDate") == None
+    assert is_not_null(results, Registration.classStartDate) == None
 
 def test_displayName():
-    assert is_not_null(results, table + "displayName") == None
+    assert is_not_null(results, Registration.displayName) == None
 
