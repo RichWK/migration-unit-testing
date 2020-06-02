af6bc792678175a296ccfc9d2b73e8d0145e5d71
Rich King <rking@rebgv.org>
2020-06-01 17:13:30 -0700

    Re-ordered to be alphabetical

--- table_definitions.py
+++ table_definitions.py
@@ -8,6 +8,35 @@ base = declarative_base()
 
 # These are ordered according to the sequence they need to be inserted into Dynamics.
 
+class ClassAttendance(base):
+    __tablename__ = 'TEMP_CLASS_ATTENDANCE'
+
+    reason = Column(Integer)
+    passed = Column(Integer)
+    checkInDate = Column(DateTime)
+    checkOutDate = Column(DateTime)
+    classID = Column(String)
+    classSessionID = Column(String)
+    contactID = Column(String)
+    statusReason = Column(Integer)
+    name = Column(String, primary_key=True)
+    attendanceNote = Column(String)
+    registrationComponent = Column(String)
+    registration = Column(String)
+    displayName = Column(String)
+
+class ClassAttendanceLog(base):
+    __tablename__ = 'TEMP_CLASS_ATTENDANCE_LOG'
+
+    checkInOut = Column(Integer)
+    checkInOutDateTime = Column(DateTime)
+    classSessionID = Column(String)
+    contactID = Column(String)
+    name = Column(String, primary_key=True)
+    classAttendanceID = Column(String)
+    statusCode = Column(Integer)
+    displayName = Column(String)
+
 class ClassPriceRule(base):
     __tablename__ = 'TEMP_CLASS_PRICE_RULE'
 
@@ -36,6 +65,67 @@ class ClassSession(base):
     sessionNumber = Column(Integer)
     classComponentID = Column(String)
     displayName = Column(String)
+
+class ContactCourseDetail(base):
+    __tablename__ = 'TEMP_CONTACT_COURSE_DETAIL'
+
+    alignmentDate = Column(DateTime)
+    attendanceResult = Column(Integer)
+    classID = Column(String)
+    completionDate = Column(DateTime)
+    contactID = Column(String)
+    courseID = Column(String)
+    courseResult = Column(Integer)
+    deliveryMethod = Column(Integer)
+    educationContactRole = Column(Integer)
+    name = Column(String, primary_key=True)
+    noComponentsPassed = Column(Integer)
+    providerForThisClassID = Column(String)
+    reasonForCourse = Column(Integer)
+    registrationID = Column(String)
+    resultReason = Column(Integer)
+    source = Column(Integer)
+    startDate = Column(DateTime)
+    statusReason = Column(Integer)
+    counter = Column(Integer)
+    components = Column(Integer)
+    applyCredits = Column(Integer)
+    displayName = Column(String)
+
+class EducationHistory(base):
+    __tablename__ = 'TEMP_EDUCATION_HISTORY'
+
+    accredUnitType = Column(Integer)
+    completedCourseUnits = Column(Integer)
+    complianceDetailID = Column(String)
+    complianceID = Column(String)
+    contactID = Column(String)
+    courseAccreditationID = Column(String)
+    courseID = Column(String)
+    courseUnits = Column(Float)
+    eduHistoryID = Column(String)
+    name = Column(String, primary_key=True)
+    programAccreditationID = Column(String)
+    programIterationID = Column(String)
+    statusReason = Column(Integer)
+    displayName = Column(String)
+    applied = Column(Integer)
+    applyCredits = Column(Integer)
+    pending = Column(Integer)
+
+class NonComplianceNotification(base):
+    __tablename__ = 'TEMP_NON_COMPLIANCE_NOTIFICATION'
+
+    comment = Column(String)
+    complianceID = Column(String)
+    contactID = Column(String)
+    name = Column(String, primary_key=True)
+    nonComplianceRule = Column(String)
+    notificationDate = Column(DateTime)
+    notificationIssue = Column(Integer)
+    stateCode = Column(Integer)
+    statuReason = Column(Integer)
+    displayName = Column(Integer)
 
 class Registration(base):
     __tablename__ = 'TEMP_REGISTRATION'
@@ -78,32 +168,6 @@ class Registration(base):
     classStartDate = Column(DateTime)
     displayName = Column(String)
 
-class ContactCourseDetail(base):
-    __tablename__ = 'TEMP_CONTACT_COURSE_DETAIL'
-
-    alignmentDate = Column(DateTime)
-    attendanceResult = Column(Integer)
-    classID = Column(String)
-    completionDate = Column(DateTime)
-    contactID = Column(String)
-    courseID = Column(String)
-    courseResult = Column(Integer)
-    deliveryMethod = Column(Integer)
-    educationContactRole = Column(Integer)
-    name = Column(String, primary_key=True)
-    noComponentsPassed = Column(Integer)
-    providerForThisClassID = Column(String)
-    reasonForCourse = Column(Integer)
-    registrationID = Column(String)
-    resultReason = Column(Integer)
-    source = Column(Integer)
-    startDate = Column(DateTime)
-    statusReason = Column(Integer)
-    counter = Column(Integer)
-    components = Column(Integer)
-    applyCredits = Column(Integer)
-    displayName = Column(String)
-
 class RegistrationComponent(base):
     __tablename__ = 'TEMP_REGISTRATION_COMPONENT'
 
@@ -126,68 +190,4 @@ class RegistrationComponent(base):
     sequence = Column(Integer)
     statusReason = Column(Integer)
     displayName = Column(String)
-
-class ClassAttendance(base):
-    __tablename__ = 'TEMP_CLASS_ATTENDANCE'
-
-    reason = Column(Integer)
-    passed = Column(Integer)
-    checkInDate = Column(DateTime)
-    checkOutDate = Column(DateTime)
-    classID = Column(String)
-    classSessionID = Column(String)
-    contactID = Column(String)
-    statusReason = Column(Integer)
-    name = Column(String, primary_key=True)
-    attendanceNote = Column(String)
-    registrationComponent = Column(String)
-    registration = Column(String)
-    displayName = Column(String)
-
-class ClassAttendanceLog(base):
-    __tablename__ = 'TEMP_CLASS_ATTENDANCE_LOG'
-
-    checkInOut = Column(Integer)
-    checkInOutDateTime = Column(DateTime)
-    classSessionID = Column(String)
-    contactID = Column(String)
-    name = Column(String, primary_key=True)
-    classAttendanceID = Column(String)
-    statusCode = Column(Integer)
-    displayName = Column(String)
-
-class EducationHistory(base):
-    __tablename__ = 'TEMP_EDUCATION_HISTORY'
-
-    accredUnitType = Column(Integer)
-    completedCourseUnits = Column(Integer)
-    complianceDetailID = Column(String)
-    complianceID = Column(String)
-    contactID = Column(String)
-    courseAccreditationID = Column(String)
-    courseID = Column(String)
-    courseUnits = Column(Float)
-    eduHistoryID = Column(String)
-    name = Column(String, primary_key=True)
-    programAccreditationID = Column(String)
-    programIterationID = Column(String)
-    statusReason = Column(Integer)
-    displayName = Column(String)
-    applied = Column(Integer)
-    applyCredits = Column(Integer)
-    pending = Column(Integer)
-
-class NonComplianceNotification(base):
-    __tablename__ = 'TEMP_NON_COMPLIANCE_NOTIFICATION'
-
-    comment = Column(String)
-    complianceID = Column(String)
-    contactID = Column(String)
-    name = Column(String, primary_key=True)
-    nonComplianceRule = Column(String)
-    notificationDate = Column(DateTime)
-    notificationIssue = Column(Integer)
-    stateCode = Column(Integer)
-    statuReason = Column(Integer)
-    displayName = Column(Integer)
 
