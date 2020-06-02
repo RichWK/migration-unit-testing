dafce33c5da6b2fbbed62ab190fd80c0bde0b1b0
Rich King <rking@rebgv.org>
2020-06-01 17:14:43 -0700

    Comment

--- program.py
+++ program.py
@@ -13,6 +13,8 @@ def is_not_null(results, column):
 
 
 
+# TODO: see https://docs.sqlalchemy.org/en/13/orm/tutorial.html#using-exists
+
 def exists_in(column1, column2):
     """Determines whether the first set of results exists in the second set.
     
@@ -21,4 +23,3 @@ def exists_in(column1, column2):
     """
 
     return None
-
