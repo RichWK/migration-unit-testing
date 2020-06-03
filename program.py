d8219d418485df5dbcff658cc24667320ed69ab3
Rich King <rking@rebgv.org>
2020-06-03 15:05:33 -0700

    Building out exists_in_target

--- program.py
+++ program.py
@@ -13,12 +13,18 @@ def is_not_null(data, column):
 
 # todo: see https://docs.sqlalchemy.org/en/13/orm/tutorial.html#using-exists
 
-def exists_in_target(session, source_column, target_column, session2 = None):
+def exists_in_target(primary_session, source_column, target_column, session2 = None):
     """Determines whether the values from the source column exist in the target.
     
     INPUT:
     column1 & column2: The single-column results of a SQLAlchemy query.
     """
+
+    # Default session2 to the primary_session if session2 is null.
+    session2 = primary_session if session2 == None else session2
+
+    source_data = primary_session.query(source_column)
+    target_data = session2.query(target_column)
 
     return None
 
