a939fc989cf1fa90fe0c9580ede6d810f70a5187
Rich King <rking@rebgv.org>
2020-06-03 17:18:11 -0700

    Docstring changes and tidying up

--- program.py
+++ program.py
@@ -4,8 +4,11 @@ def is_not_null(data, column):
     """Determines whether the provided column contains any null values.
     
     INPUT:
-    data: This argument should be the results of a SQLAlchemy query.
+    data: This should be the results of a SQLAlchemy query.
     column: A string specifying the table and column name, separated with only a period.
+
+    OUTPUT:
+    Boolean value.
     """
 
     if data.filter(column == None).first() == None:
@@ -16,25 +19,26 @@ def is_not_null(data, column):
 # todo: see https://docs.sqlalchemy.org/en/13/orm/tutorial.html#using-exists
 
 def missing_from_target(source_column, target_column, primary_session, session2 = None):
-    """Determines whether the values from the source column exist in the target.
+    """Determines whether the values from the source column exist in the target column.
     
     INPUT:
-    column1 & column2: The single-column results of a SQLAlchemy query.
+    source_column: The values to be verified, in the format of tablename.columnname
+    target_column: The name of the column which values will be verified against.
+    primary_session: This determines the connection used. (See connections.py.)
+    session2: This is used as the connection for 'target_column' if provided.
+
+    OUTPUT:
+    An integer with a count of the number of missing records.
     """
 
-    # Default session2 to the primary_session if it hasn't been provided.
+    # Default session2 to the primary_session (but only if it hasn't been provided).
     session2 = primary_session if session2 == None else session2
 
     source_data = primary_session.query(source_column)
     target_data = session2.query(target_column)
 
-    # source_data.filter(exists().where(source_column==target_column))
-
     total_count = source_data.count()
     exists_count = 0
-
-    # for name in source_data.filter(source_column.notin_(target_column)):
-    #     exists_count += 1
 
     for name in source_data.filter(~exists().where(source_column==target_column)):
         exists_count += 1
