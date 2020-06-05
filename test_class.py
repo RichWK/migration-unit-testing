3c657555fa58d18ce6b51f01b3cfe9622127d5f3
Rich King <rking@rebgv.org>
2020-06-05 10:44:44 -0700

    Added Class checks

---
+++ test_class.py
@@ -0,0 +1,22 @@
+from connections import *
+from program import *
+from table_definitions import *
+
+scribe_dev1 = SCRIBE_DEV1.session
+
+# These tests verify that lookups to other entities actually exist in those entities.
+
+def test_course_lookup():
+    assert missing_from_target(
+        Class.courseID
+        ,Course.name
+        ,scribe_dev1
+    ) == 0
+
+def test_venue_lookup():
+    assert missing_from_target(
+        Class.venueID
+        ,Venue.name
+        ,scribe_dev1
+    ) == 0
+
