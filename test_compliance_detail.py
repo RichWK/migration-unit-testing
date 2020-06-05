f4f88ac34243156ee52d8b48678131321ddd2f9c
Rich King <rking@rebgv.org>
2020-06-05 14:38:40 -0700

    Added checks for Compliance Detail & Course Accred

---
+++ test_compliance_detail.py
@@ -0,0 +1,15 @@
+from connections import *
+from program import *
+from table_definitions import *
+
+scribe_dev1 = SCRIBE_DEV1.session
+
+# These tests verify that lookups to other entities actually exist in those entities.
+
+def test_compliance_lookup():
+    assert missing_from_target(
+        ComplianceDetail.complianceID
+        ,Compliance.name
+        ,scribe_dev1
+    ) == 0
+
