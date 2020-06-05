08b7ab35c96a4de4c187788eebd57f7a14865821
Rich King <rking@rebgv.org>
2020-06-05 11:56:40 -0700

    New Course Restriction entity added for testing

---
+++ test_course_restriction.py
@@ -0,0 +1,22 @@
+from connections import *
+from program import *
+from table_definitions import *
+
+scribe_dev1 = SCRIBE_DEV1.session
+
+# These tests verify that lookups to other entities actually exist in those entities.
+
+def test_class_lookup():
+    assert missing_from_target(
+        CourseRestriction.classID
+        ,Class.mmsiClassCode
+        ,scribe_dev1
+    ) == 0
+
+def test_course_lookup():
+    assert missing_from_target(
+        CourseRestriction.courseID
+        ,Course.name
+        ,scribe_dev1
+    ) == 0
+
