5bac4ce8570d02c9fd2e9e2274ed5927e31c4b11
Rich King <rking@rebgv.org>
2020-06-08 11:43:17 -0700

    Added null and duplicate checks

--- test_class_contact.py
+++ test_class_contact.py
@@ -3,6 +3,10 @@ from program import *
 from table_definitions import *
 
 scribe_dev1 = SCRIBE_DEV1.session
+data = scribe_dev1.query(ClassContact)
+
+def test_duplicates():
+    assert duplicates_exist(scribe_dev1, ClassContact.name) == False
 
 # These tests verify that lookups to other entities actually exist in those entities.
 
@@ -27,4 +31,24 @@ def test_courseContact_lookup():
         ,CourseContact.name
         ,scribe_dev1
     ) == 0
+
+# Everything below checks for nulls:
+
+def test_classID():
+    assert is_not_null(data, ClassContact.classID) == True
+
+def test_contactID():
+    assert is_not_null(data, ClassContact.contactID) == True
+
+def test_courseID():
+    assert is_not_null(data, ClassContact.courseID) == True
+
+def test_roleForThisClass():
+    assert is_not_null(data, ClassContact.roleForThisClass) == True
+
+def test_courseContactID():
+    assert is_not_null(data, ClassContact.courseContactID) == True
+
+def test_displayName():
+    assert is_not_null(data, ClassContact.displayName) == True
 
