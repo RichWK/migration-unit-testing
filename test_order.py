e9449fd7cd8690832526733797a3b2b202f8293a
Rich King <rking@rebgv.org>
2020-06-12 11:47:37 -0700

    Line spacing change

--- test_order.py
+++ test_order.py
@@ -7,9 +7,5 @@ scribe_dev1 = SCRIBE_DEV1.session
 # These tests verify that lookups to other entities actually exist in those entities.
 
 def test_order_lookup():
-    assert missing_from_target(
-        Order.orderNo
-        ,Registration.orderID
-        ,scribe_dev1
-    ) == 0
+    assert missing_from_target(Order.orderNo, Registration.orderID, scribe_dev1) == 0
 
