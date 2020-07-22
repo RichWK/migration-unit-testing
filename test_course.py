164e460c3bba9254a77f454799a9063e8df56341
Rich King <rking@rebgv.org>
2020-07-21 17:50:58 -0700

    Added tests for the Course entity

---
+++ test_course.py
@@ -0,0 +1,24 @@
+from connections import *
+from program import *
+from table_definitions import *
+
+scribe_dev1 = SCRIBE_DEV1.session
+data = scribe_dev1.query(Course)
+
+# These tests verify that lookups to other entities actually exist in those entities.
+
+def test_primaryAccreditation_lookup():
+    assert missing_from_target(
+        Course.primaryAccreditation
+        ,CourseAccreditation.name
+        ,scribe_dev1
+    ) == 0
+
+# Everything below checks for nulls:
+
+def test_name():
+    assert is_not_null(data, Course.name) == True
+
+def test_primaryAccreditation():
+    assert is_not_null(data, Course.primaryAccreditation) == True
+
